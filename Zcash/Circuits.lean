14db5935284104a7d75336c6d61bd0b49f7ba677
Gregor <gregor.mitscha-baude@gmx.at>
2026-07-31 18:16:33 +0200

    Integrate Clean keygen lawfulness

--- Zcash/Circuits.lean
+++ Zcash/Circuits.lean
@@ -84,7 +84,6 @@ import Zcash.Circuits.Action.Spec
 import Zcash.Circuits.Action.Separation
 import Clean.Halo2.TopLevel
 import Zcash.Circuits.Action.TopLevel
-import Zcash.Circuits.Action.SelectorCoherence
 import Zcash.Circuits.Integration
 
 /-!
